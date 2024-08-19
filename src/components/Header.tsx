f1b644f3f7b1f87a4cf664b065a2017a296b1d23
jefri agistar <jefri4876@gmail.com>
2024-08-19 18:57:12 +0700

    feat: create header novel library

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header: React.FC = () => {
+  return (
+    <header className="bg-gray-800 text-white text-center py-4">
+      <h1 className="text-2xl font-bold">Novel Library</h1>
+    </header>
+  )
+}
+
+export default Header;
